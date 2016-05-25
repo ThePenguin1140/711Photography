382ee64393d83a3882563457d8eba39c160b954b
Jason Wuertz <jwuertz@upei.ca>
2016-05-25 09:05:22 -0300

    feat(index): update to use express

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(process.env.PORT, function(){
-    console.log('Server running on 8080...');
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.use('/', express.static('app'))
+
+app.listen(process.env.PORT, function() {
+    console.log('Server running on ' + process.env.PORT)
 });
