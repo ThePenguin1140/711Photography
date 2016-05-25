97ed26f077429d5ea8e29e4a4ad939b025afeb5b
Jason Wuertz <jwuertz@upei.ca>
2016-05-25 15:41:32 -0300

    feat(proj): replace angular with vanilla js
    god forgive me for this commit
    I removed angular and replaced it with vanilla js
    things acutally work now
    add bullseye.js to center images

--- app/app.js
+++ app/app.js
@@ -1,12 +1,30 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+$(function () {
+    var gridster = $(".gridster ul").gridster({
+        widget_margins: [10, 10],
+        widget_base_dimensions: [50, 50],
+        resize: {
+            enabled: true,
+            resize: function( e, ui, $widget ) {
+                if( $widget.width() > $widget.height() ) {
+                    $widget.children('img').addClass('landscape').removeClass('portrait');
+                } else {
+                    $widget.children('img').addClass('portrait').removeClass('landscape');
+                }
+            },
+            stop: function( e, ui, $widget ) {
+                $widget.children('div').bullseye({
+                    'fadeEffect': false
+                });
+            }
+        },
+        avoid_overlapped_widgets: true
+    }).data('gridster');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    $.get('/images', function (res) {
+        _(_.split(_.trim(res, '[]'), ',')).forEach(function (img, i) {
+            gridster.add_widget('<li class="new"><div class="portrait landscape"><img class="portrait" src="/images/' + _.trim(img, '"') + '"/></div></li>', 2, 2, 0, 0);
+        });
+    });
+});
