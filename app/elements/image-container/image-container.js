299e59e7fda6c1f8a1b66ad5f8e065b2564a728b
Jason Wuertz <jwuertz@upei.ca>
2016-05-28 12:56:48 -0300

    feat(image-container): split html and js files

---
+++ app/elements/image-container/image-container.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Jared on 5/28/2016.
+ */
+Polymer({
+
+    is: 'image-container',
+
+    properties: {
+        url: {
+            type: String,
+            value: 'image-container',
+        },
+    },
+
+});
