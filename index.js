776dfe552d9b9215cc20a8817b0e7a96635deff3
Jason Wuertz <jwuertz@unb.ca>
2016-05-30 17:42:06 -0300

    feat(index.js): update state getter for gridstack

--- index.js
+++ index.js
@@ -15,10 +15,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/state', function (req, res, next) {
-    console.log(lastState);
-    fs.readdir('public/images', function (err, items) {
-        if (_.isUndefined(lastState)) {
-            console.log('populating state');
+    if (_.isUndefined(lastState)) {
+        console.log("initializing state");
+        fs.readdir('public/images', function (err, items) {
             lastState = _.fill(
                 Array(items.length),
                 {
@@ -27,26 +26,22 @@ app.get('/state', function (req, res, next) {
                     size_x: 2,
                     size_y: 2
                 }
-            )
-        }
-
-        console.log(_.merge(
-            _.map(items, function (item) {
-                return {'url': item}
-            }),
-            lastState
-        ));
-        res.send(
-            _.merge(
+            );
+            lastState = _.merge(
                 _.map(items, function (item) {
                     return {'url': item}
                 }),
                 lastState
-            )
-        );
-
+            );
+            console.log("sending state:\n" + JSON.stringify(lastState));
+            res.send(lastState);
+            next;
+        });
+    } else {
+        console.log("sending state:\n" + JSON.stringify(lastState));
+        res.send(lastState);
         next;
-    });
+    }
 });
 
 app.post('/state', function (req, res, next) {
