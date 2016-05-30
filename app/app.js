77a672f748b84d659e1645247a204133e0a5fe6e
Jason Wuertz <jwuertz@upei.ca>
2016-05-30 12:55:23 -0300

    refactor(app.js): remove old code

--- app/app.js
+++ app/app.js
@@ -6,28 +6,6 @@ $(function () {
         cellHeight: 40
     });
 
-    // var gridster = $(".gridster ul").gridster({
-    //     widget_selector: "image-container",
-    //     widget_margins: [10, 10],
-    //     widget_base_dimensions: [50, 50],
-    //     resize: {
-    //         enabled: true,
-    //         resize: function (e, ui, $widget) {
-    //             if ($widget.width() > $widget.height()) {
-    //                 $widget.children('img').addClass('landscape').removeClass('portrait');
-    //             } else {
-    //                 $widget.children('img').addClass('portrait').removeClass('landscape');
-    //             }
-    //         },
-    //         stop: function (e, ui, $widget) {
-    //             $widget.children('div').bullseye({
-    //                 'fadeEffect': false
-    //             });
-    //         }
-    //     },
-    //     avoid_overlapped_widgets: true
-    // }).data('gridster');
-
     var goHome = function () {
 
     };
@@ -36,33 +14,13 @@ $(function () {
 
     };
 
-    // $('#saveButton').click(function () {
-    //     $.post(
-    //         '/state',
-    //         {state: gridster.serialize()},
-    //         function (data) {
-    //             console.log(data);
-    //         }
-    //     );
-    // });
-    //
-    // $('#loadButton').click(function () {
-    //     $.get('/state', function (res) {
-    //         _(res).forEach(function (item) {
-    //             gridster.add_widget(
-    //                 '<li class="new">' +
-    //                 '<div class="portrait landscape">' +
-    //                 '<img class="portrait" src="/images/' + item.url + '"/>' +
-    //                 '</div>' +
-    //                 '</li>',
-    //                 item.size_x,
-    //                 item.size_y,
-    //                 item.col,
-    //                 item.row
-    //             );
-    //         });
-    //     });
-    // })
+    $('#saveButton').click(function () {
+
+    });
+
+    $('#loadButton').click(function () {
+
+    });
 
     $.get('/state', function (res) {
         var grid = $('.grid-stack').data('gridstack');
